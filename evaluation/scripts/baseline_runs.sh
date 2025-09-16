ab9840ede02db256a30712816164f7b0969985d2
Koyena Pal <pal.k@northeastern.edu>
2025-09-16 12:33:53 +0000

    current set of runs in misc

--- evaluation/scripts/baseline_runs.sh
+++ evaluation/scripts/baseline_runs.sh
@@ -2,8 +2,8 @@
 
 # Define arrays of models and prompt styles
 #models=("Qwen/QwQ-32B" "BytedTsinghua-SIA/DAPO-Qwen-32B" "open-thoughts/OpenThinker-7B")
-#models=("openai/gpt-oss-20b")
-models=("Qwen/QwQ-32B" "open-thoughts/OpenThinker-7B" "BytedTsinghua-SIA/DAPO-Qwen-32B" "nvidia/Nemotron-Research-Reasoning-Qwen-1.5B")
+models=("openai/gpt-oss-20b")
+#models=("Qwen/QwQ-32B" "open-thoughts/OpenThinker-7B" "BytedTsinghua-SIA/DAPO-Qwen-32B" "nvidia/Nemotron-Research-Reasoning-Qwen-1.5B")
 targetmodels=("openai/gpt-oss-20b" "open-thoughts/OpenThinker-7B" "BytedTsinghua-SIA/DAPO-Qwen-32B" "nvidia/Nemotron-Research-Reasoning-Qwen-1.5B" "Qwen/QwQ-32B")
 # reasoningeffort= ("low" "medium" "high")
 mkdir -p logs
@@ -17,7 +17,7 @@ for model in "${models[@]}"; do
     # Sanitize names for filenames
     safe_model="${model//\//_}"
     safe_targetmodel="${targetmodel//\//_}"
-    outputpath="without_answer/${safe_model}_zero_shot_original_without_answer.jsonl"
+    outputpath="without_answer/${safe_model}_zero_shot_original_low_without_answer.jsonl"
     timestamp=$(date +"%Y%m%d_%H%M%S")
     logfile="logs/${safe_model}_to_${safe_targetmodel}_$timestamp.log"
     echo "Running: python run_transfer_thoughts.py --model $model --target_model $targetmodel --prompt zero_shot --source_model_output_file $outputpath --reasoning_effort low"
